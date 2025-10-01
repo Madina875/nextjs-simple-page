33b5d8f02123686db6bfb9331c83e59ff8ad9ddf
madina875 <evaevasmq@gmail.com>
2025-10-01 21:45:57 +0500

    hmw

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Carousel from "antd/es/carousel";
 import React from "react";
 
 const contentStyle: React.CSSProperties = {
-  height: "200px",
+  height: "300px",
   color: "black",
-  lineHeight: "200px",
+  lineHeight: "300px",
   textAlign: "center",
   background: "#f3e8ff",
 };
@@ -14,20 +14,34 @@ const Home = () => {
   return (
     <div className="min-h-screen">
       <div>
-        <Carousel autoplay>
+        <Carousel className="font-[Tangerine] text-2xl" autoplay>
           <div>
-            <h3 style={contentStyle}>1</h3>
+            <h3 style={contentStyle}>Home</h3>
           </div>
           <div>
-            <h3 style={contentStyle}>2</h3>
+            <h3 style={contentStyle}>Users</h3>
           </div>
           <div>
-            <h3 style={contentStyle}>3</h3>
+            <h3 style={contentStyle}>Login page</h3>
           </div>
           <div>
-            <h3 style={contentStyle}>4</h3>
+            <h3 style={contentStyle}>Products</h3>
           </div>
         </Carousel>
+      </div>
+
+      <div className="container pt-5 font-[Volkhov]">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+          quia pariatur harum necessitatibus error, facilis, itaque tempora
+          facere, corporis aspernatur consequuntur reprehenderit voluptatem
+          vitae voluptas nobis corrupti praesentium accusantium dignissimos
+          eaque consectetur quae. Iste, tempore iure necessitatibus alias sed
+          amet pariatur quam architecto facilis voluptas accusantium corporis
+          laborum ullam a repellendus perferendis ex. Quisquam perferendis,
+          aperiam quibusdam consequatur laborum repellendus at non? Repellat,
+          aut iure eius ad similique voluptatibus sint!
+        </p>
       </div>
     </div>
   );
